e6c0dea02b6f6b5b9b047d64dff8dc24286742fb
Kostas <kmouts@hotmail.com>
2023-01-25 13:53:39 +0200

    Save GT images with annotations

--- check_data_km/my_evaluate_on_SNv3.py
+++ check_data_km/my_evaluate_on_SNv3.py
@@ -4,6 +4,7 @@ import pickle
 import sys
 from collections import defaultdict
 from pprint import pprint
+import copy
 
 import cv2
 import torch
@@ -13,6 +14,8 @@ from torchmetrics.detection.mean_ap import MeanAveragePrecision
 import numpy as np
 
 from data.SNv3_dataloader import create_snv3_dataset
+from data.SoccerNet.visualize import torch2cv2
+from data.augmentation import tensor2image
 from data.data_reader import my_collate
 from data.issia_dataset_km import P_IssiaDataset, create_issia_dataset
 from data.issia_utils import evaluate_ball_detection_results, _ball_detection_stats, ball_boxes_to_centers_list
@@ -27,6 +30,7 @@ ball_confidence_threshold = 0.7
 player_confidence_threshold = 0.7
 my_device = 'cuda'
 snv3_dataset_path = '/mnt/DATA/DATASETS/SOCCERNETv3/SNV3/SNV3_PIP_data_final'
+snv3_tmp = '/mnt/DATA/DATASETS/SOCCERNETv3/tmp/'
 
 only_ball_frames = False
 
@@ -60,6 +64,7 @@ model.eval()
 metric = MeanAveragePrecision(iou_type="bbox", class_metrics=True, compute_on_cpu=True,
                               iou_thresholds=[0.5], rec_thresholds=list(np.linspace(0, 1, 11))
                               ).to(torch.device("cpu"))
+metric2 = MeanAveragePrecision(iou_type="bbox", class_metrics=True, compute_on_cpu=True).to(torch.device("cpu"))
 
 count_batches = 0
 # ball_pos contains list of ball positions (x,y) on each frame; multiple balls per frame are possible
@@ -84,6 +89,7 @@ for ndx, (images, boxes, labels) in enumerate(tqdm(dataloaders[phase])):
 
     # Update metric with predictions and respective ground truth
     metric.update(pred_cpu, target)
+    metric2.update(pred_cpu, target)
 
     torch.cuda.empty_cache()
 
@@ -91,10 +97,32 @@ for ndx, (images, boxes, labels) in enumerate(tqdm(dataloaders[phase])):
     for t in pred_cpu:
         ball_boxes_to_centers_list(t, pred_ball_pos)
 
+    # Visualize gt and predictions
+    if count_batches % 100 == 0:
+    # Person bounding boxes
+    #     frame = copy.deepcopy(images[0]).numpy(force=True)
+        frame = tensor2image(images[0]) * 255
+        # frame = torch2cv2(images[0].cpu()) * 255
+        # frame = frame.transpose(1, 2, 0)
+        for (x1, y1, x2, y2), lb in zip((np.rint(boxes[0].numpy(force=True))).astype(int), labels[0].numpy(force=True)):
+            color = (0, 0, 0)
+            if lb == 1:  # ball
+                color = (0, 0, 153)
+            elif lb == 2:  # player
+                color = (204, 0, 0)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color)
+            # cv2.imshow('frame', cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
+            cv2.imwrite(snv3_tmp + 'batch_' + str(count_batches) + '_0.png', frame)
+
 # Compute the results
 result = metric.compute()
 pprint(result)
 print(result.map.numpy())
+print("---------------------------------------------------")
+# Compute the results2
+result2 = metric2.compute()
+pprint(result2)
+print(result2.map.numpy())
 
 # https://chtalhaanwar.medium.com/pytorch-num-workers-a-tip-for-speedy-training-ed127d825db7
 
@@ -113,7 +141,6 @@ avg_precision = sum(temp) / len(temp)
 
 temp = [r for (_, r, _) in frame_stats if r is not None]
 avg_recall = sum(temp) / len(temp)
-
 
 print('Avg. ball precision (tolerance: ' + str(tolerance) + ' pixels) = ' + str(avg_precision))
 print('Avg. ball recall = ' + str(avg_recall))
